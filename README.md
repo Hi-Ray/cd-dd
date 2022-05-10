84da155b7e3d2132db380b34cf421d0974916bf6
Ray <dev@hiray.me>
2022-05-10 01:54:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Community Dragon Directory Downloader
+
+## IMPORTANT
+Your urls must end in a "/" you should just copy and paste your urls into the command line
+
+## Requirements
+    - Git
+    - Nodejs
+    - Yarn (Optional but recommended)
+
+## Usage
+```shell
+> git clone https://github.com/Hi-Ray/cd-dd.git
+
+> yarn # or you can use "npm install"
+
+> yarn start <url> # or "npm start <url>"
+```
+
+## Usage example
+
+```shell
+> yarn start https://raw.communitydragon.org/latest/game/data/images/
+```
---
+++ index.ts
@@ -0,0 +1,63 @@
+import Axios from "axios";
+import Download from 'download'
+import fs from 'fs'
+
+import {Command} from "commander";
+import {colorConsole} from "tracer";
+
+
+const logger = colorConsole()
+
+const downloadFolder = (URL: string) => {
+    // Check if it's a community dragon link
+    if (!URL.includes('communitydragon.org')) {
+        logger.fatal('URL is not a valid CommunityDragon link.')
+        return
+    }
+
+    // Check if it's a folder
+    if (!URL.endsWith('/')) {
+        logger.fatal('URL is not a folder.')
+        return
+    }
+    // Foir now set a default
+    let startingStr = URL;
+
+    // Strip the "https://"
+    if (URL.startsWith('https://')) {
+        startingStr = URL.split('//')[1]
+    }
+
+    // remove Community dragon temporarily
+    let removeCdrag = startingStr.split('/')
+
+    removeCdrag.shift()
+
+    // Re add the raw link with the json token
+    removeCdrag.unshift('raw.communitydragon.org', 'json')
+
+    // Add back in
+    let jsonifiedString = `https://${removeCdrag.join('/')}`
+
+    // Make a dir
+    fs.mkdirSync('out')
+
+    // Download le files.
+    Axios.get(jsonifiedString).then(({data}) => {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].type !== 'file'){
+                continue;
+            }
+            Download(`${URL}${data[i].name}`).pipe(fs.createWriteStream(`out/${data[i].name}`));
+        }
+    })
+
+}
+
+const program = (new Command())
+    .argument('<URL>', 'URL starting with https://')
+    .action((url: string) => downloadFolder(url))
+    .version('1.0.0');
+
+// Parse the CLI
+program.parse()
